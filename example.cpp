98453ffccf9565ee04619bd8d7330d9d1918bcc9
Zhu Shangshu <83390079+20191390@users.noreply.github.com>
2025-11-02 01:12:38 +0800

    添加注释
    对主函数添加任务那块加了点注释

--- example.cpp
+++ example.cpp
@@ -6,8 +6,8 @@ int main()
     std::vector<std::future<int>> results;
 
     for (int i = 0; i < 8; ++i) {
-        results.emplace_back(
-            pool.enqueue([i] {
+        results.emplace_back( // 保存每个异步结果
+            pool.enqueue([i] { // 将每个任务插入到任务队列里, 每个任务的功能均为: 打印+睡眠1s+打印+返回结果
                 std::cout << "hello " << i << std::endl;
                 std::this_thread::sleep_for(std::chrono::seconds(1));
                 std::cout << "world " << i << std::endl;
@@ -16,7 +16,7 @@ int main()
         );
     }
 
-    for (auto&& result : results)
+    for (auto&& result : results) // 一次性取出保存在results中的异步结果
         std::cout << result.get() << ' ';
     std::cout << std::endl;
 
